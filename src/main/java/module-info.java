69afc003eebce323edd9d795c3fa9ae2d2447364
adrianc68 <angeladriancamalgarcia@hotmail.com>
2022-05-06 00:20:07 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.uvgram.uvgramdesktop {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.uvgram.uvgramdesktop to javafx.fxml;
+    exports com.uvgram.uvgramdesktop;
+}
